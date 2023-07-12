52f1b6f21232fd4b33ea5fb03b23724f694ac5ce
novalastix <10227076+novalastix@users.noreply.github.com>
2023-07-12 15:41:08 -0700

    Add roll command

---
+++ commands/roll.js
@@ -0,0 +1,69 @@
+const {SlashCommandBuilder,EmbedBuilder} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('roll')
+		.setDescription('Roll a dice')
+        .addStringOption(option => option.setName('custom').setDescription("Enter a custom dice roll separated by commas (e.g. '2d8,1d20'").setRequired(false)),
+	async execute(interaction) {
+        const name = interaction.member.toString(); //interaction.member.nickname || interaction.member.user.username;
+        const input = interaction.options.getString('custom') || "1d20";
+
+        let message = name + " rolled " + input + "\n#";
+        let total = 0;
+        let count = 0;
+        try
+        {
+            if(input.length > 100) throw new Error('Input string cannot be too long.');
+
+            let inputRaw = input.replace(/\s+/g, '');
+            let rolls = inputRaw.split(',');
+
+            for(r in rolls)
+            {
+                let roll = rolls[r];
+                [x,y] = roll.split('d');
+                let amount = parseInt(x);
+                let sides = parseInt(y);
+
+                if(amount < 1 || amount > 100) throw new Error('Dice amount must be between 1 and 100.');
+
+                for (i = 0; i < amount; i++)
+                {
+                    if(sides < 2 || sides > 100) throw new Error('Dice sides must be between 2 and 100.');
+
+                    output = Math.floor(Math.random() * sides) + 1;
+                    total += output;
+
+                    message += " `"+output+"`";
+
+                    count++
+                }
+            }
+
+            if(count > 1)
+            {
+                message += "\nTotal: "+total;
+            }
+
+            if(count > 100)
+            {
+                throw new Error('Dice amount must be between 1 and 100.');
+            }
+            
+        }
+        catch(e)
+        {
+            await interaction.reply({content: "There was an error parsing your dice string. It should look like `2d8,1d20`",ephemeral:true});
+            return;
+        }
+
+        await interaction.deferReply({ ephemeral: false });
+
+        const rollEmbed = new EmbedBuilder()
+        .setColor(0xFFFFFF)
+        .setDescription(message);
+
+        await interaction.editReply({ embeds: [rollEmbed] });
+    }
+};
